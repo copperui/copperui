325a1ae2e75b7f68ddd99a491788375e7f8d3103
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-01-25 10:43:45 -0400

    feat(docs): replace sb with docusauros scaffold

--- packages/core/src/stories/createElement.js
+++
@@ -1,9 +0,0 @@
-export const createElement = (name, attributes) => {
-  const el = document.createElement(name);
-
-  for (const [key, value] of Object.entries(attributes)) {
-    el.setAttribute(key, value);
-  }
-
-  return el.outerHTML;
-};
