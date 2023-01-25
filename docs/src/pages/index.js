26e3a90cacae8cbf18a42d10b43ddb435fcca001
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-01-25 17:03:20 -0400

    docs: improve começar agora

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/comecar-agora">
+          <Link className="button button--secondary button--lg" to="/docs/comecar-agora/introducao">
             Começar Agora
           </Link>
         </div>
